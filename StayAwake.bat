0eb65c3ec93ab6204f4c720e36f448c85da18277
Ben <ben@tennant.family>
2016-08-15 17:45:10 -0500

    Initial Upload
    This is a very simple script that uses the powercfg.exe command to
    disable sleep on a Windows computer. I originally created this script
    to keep devices online and awake during a deployment project where
    workstations were acting as “servers” and needed to always be online.

---
+++ StayAwake.bat
@@ -0,0 +1,10 @@
+::Stayawake.bat
+::Simple script to disable sleep and hibernate behaviors on workstations.
+powercfg.exe -change -monitor-timeout-ac 0
+powercfg.exe -change -monitor-timeout-dc 0
+powercfg.exe -change -disk-timeout-ac 0
+powercfg.exe -change -disk-timeout-dc 0
+powercfg.exe -change -standby-timeout-ac 0
+powercfg.exe -change -standby-timeout-dc 0
+powercfg.exe -change -hibernate-timeout-ac 0
+powercfg.exe -change -hibernate-timeout-dc 0
